f14b21a1b8701268419475e235474149d678a344
EduardoMunozN <124702009+EduardoMunozN@users.noreply.github.com>
2023-03-04 01:04:01 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva1_15_sobrecarga;
+
+/**
+ *
+ * @author eduar
+ */
+public class EVA1_15_SOBRECARGA {
+
+    public static void main(String[] args) {
+        //diferentes println (10)
+        //métodos para imprimir diferentes tipos de datos
+        //para crear sobrecarga de métodos la firma del método debes ser única
+        System.out.println("Hello World!");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+        
+
+
+
+    }
+}
